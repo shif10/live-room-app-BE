8ad1c4737a87b8f72f0e5e0f17ef71cd202c9dbd
shifa <shifa.b@thinkwik.com>
2025-11-28 13:18:08 +0530

    added origin

--- socket.ts
+++ socket.ts
@@ -1,38 +1,34 @@
-
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { Server } from "socket.io";
 
 let io;
 
 export const initSocket = (server) => {
-   io = new Server(server, {
+  io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://main.d16zm1qoja2vlw.amplifyapp.com/",
+      ],
       methods: ["GET", "POST"],
       credentials: true,
     },
   });
-  
 
-
-   io.use((socket,next)=>{
-
-    const token=socket.handshake.auth?.token
-    if(!token)
-    {
-       return next(new Error("Authentication Error"))
+  io.use((socket, next) => {
+    const token = socket.handshake.auth?.token;
+    if (!token) {
+      return next(new Error("Authentication Error"));
     }
 
     try {
       const user = jwt.verify(token, process.env.JWT_SECRET);
       socket.user = user;
-    
+
       next();
-    } catch (error) {
-      
-    }
-   })
-  
+    } catch (error) {}
+  });
+
   io.on("connection", (socket) => {
     console.log("Socket connected:", socket.id);
 
